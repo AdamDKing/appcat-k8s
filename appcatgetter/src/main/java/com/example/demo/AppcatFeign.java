fd8cf5834fc8c27fc666f335347d22c2d5dcb59e
AdamDKing <adamdentonking@gmail.com>
2019-12-03 19:42:28 -0500

    tried tacking port number onto feignclient url

--- appcatgetter/src/main/java/com/example/demo/AppcatFeign.java
+++ appcatgetter/src/main/java/com/example/demo/AppcatFeign.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "appcat", url = "http://appcat.default.svc.cluster.local")
+@FeignClient(name = "appcat", url = "http://appcat.default.svc.cluster.local:8080")
 public interface AppcatFeign {
 	
 	@GetMapping("/cats")
